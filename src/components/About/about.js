0105a57c722896e1d1b1de3bfe8975dce8cf7f3d
Delfiab <mdabreubo24@ucema.edu.ar>
2022-11-01 10:05:55 -0300

    commit 6

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -17,7 +17,7 @@ export const About = () => {
                     <img src={about_us} alt="Our team" />
                     <div className="all-text">
                         <p>
-                            Food talk fue creado por un grupo de personas que están buscando, creando y mejorando recetas constantemente para compartir!<br/> 
+                            Food talk fue creado por un grupo de personas que están buscando, creando y mejorando recetas constantemente para compartir con ustedes nuestros lectores!<br/> 
                             <br />Tenemos un equipo de genios detrás de nosotros en Food Talk que son expertos en un poco de todo, desde servicio al cliente, redes sociales, videografía, hasta ayudar con sesiones de recetas. Ellos son TODO. Nuestro equipo ayuda a que todo funcione sin problemas.
                             <br />En este espacio, siempre estamos creando nuevas, frescas y sabrosas recetas para vos. Nuestro objetivo es inspirarte con comidas que son fáciles y ricas, para que cocines para vos, tu familia, tus amigos o tu pareja. <br />
                             <br /><em>Actualmente cocinando en la hermosa ciudad de Buenos Aires. </em> 
